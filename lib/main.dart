f3cde9e054f0229f4cf17f4224eb0c8eacec4b04
yumayuma708 <yumaskywalker708@gmail.com>
2025-05-20 10:35:15 +0900

    通知機能を実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:nekoze_notify/nekoze_app.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _initializeNotifications();
   runApp(const NekozeApp());
+}
+
+Future<void> _initializeNotifications() async {
+  const DarwinInitializationSettings darwinSettings =
+      DarwinInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+      );
+
+  const InitializationSettings initSettings = InitializationSettings(
+    iOS: darwinSettings,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
 }
